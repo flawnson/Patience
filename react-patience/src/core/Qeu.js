4e3f6e6b8d05ac9b9508adde6babab83d3b22dd3
flawn <flawnsontong1@gmail.com>
2021-01-28 14:05:27 -0500

    Added react boilerplate code

---
+++ react-patience/src/core/Qeu.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+import FormInput from "../components/inputs/FormInput.js"
+
+
+function Qeu () {
+    return (
+        <div>
+            {/*Logo*/}
+            <logo>
+
+            </logo>
+
+            {/*Input Field*/}
+            <FormInput>
+
+            </FormInput>
+
+            {/*Enter button*/}
+            <button>
+
+            </button>
+
+        </div>
+    )
+}
