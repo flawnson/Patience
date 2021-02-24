6b946acd4608ebda947867eb9c2485719d918eab
Flawnson Tong <flawnsontong1@gmail.com>
2021-02-24 01:18:09 -0500

    Refactor landing page to class component and added state

--- react-patience/src/core/Qeu.js
+++ react-patience/src/core/Qeu.js
@@ -4,22 +4,51 @@ import FormInput from "../components/inputs/FormInput.js"
 import logo from "../Ouroboros.svg";
 
 
-export default function Qeu () {
-    return (
-        <div>
-            {/*Logo*/}
-            <img src={logo} className="App-logo" alt="logo" />
+export default class Qeu extends React.Component () {
+    constructor () {
+        super()
+        this.state = {
+            toggle: false
+        }
+        this.handleEnter = this.handleEnter.bind(this)
+        this.handleToggle = this.handleToggle.bind(this)
+    }
 
-            {/*Input field for queueing codes*/}
-            <FormInput>
+    handleEnter () {
 
-            </FormInput>
 
-            {/*Enter button*/}
-            <button>
+    }
 
-            </button>
+    handleToggle () {
+        this.setState({toggle: true})
+    }
 
-        </div>
-    )
+    render () {
+        return (
+        <div className='component-container' >
+            {
+                !this.state.toggle
+                    ?
+                    /*Logic for manual ID input*/
+                    <div className='landing-wrapper' >
+                        <div className='logo-container' >
+                            {/*Logo*/}
+                            <img src={logo} className="App-logo" alt="logo"/>
+
+                            {/*Input field for queueing codes*/}
+                            <FormInput>
+
+                            </FormInput>
+
+                            {/*Enter button*/}
+                            <button onClick={this.handleEnter}>Enter</button>
+                        </div>
+                    </div>
+                    :
+                    /*Logic for QR code scanning*/
+                    <button onClick={this.handleToggle}>QR</button>
+            }
+            </div>
+        )
+    }
 }
